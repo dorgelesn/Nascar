8eb7a3a125e56361307070c7172b80290caa07a8
DrGlobox <locap25@gmail.com>
2012-12-20 14:53:09 +0100

    delete Prototype 2

--- Prototype_2/semaphore.h
+++
@@ -1,6 +0,0 @@
-/* semaphore.h */
-extern void erreurFin(const char* msg);
-extern void initSem(int nbSem,char* nomFich,int* pvinit);
-extern void libereSem();
-extern void P(int numSem);
-extern void V(int numSem);
